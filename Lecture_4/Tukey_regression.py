333ec3f100e9d55ad06c8e9883b3a034cf6e2673
Yuki Kumon <yuki-kumon@g.ecc.u-tokyo.ac.jp>
2019-05-08 18:41:10 +0900

    とりあえず計算部分はだいたい書いたかな？

--- Lecture_4/Tukey_regression.py
+++ Lecture_4/Tukey_regression.py
@@ -32,28 +32,38 @@ def build_design_matrix(x):
     return phi
 
 
-def predict(x, theta):
-    phi = build_design_matrix(x)
-    return phi.dot(theta)
-
-
 def loop(x, y, phi, theta, eta):
     '''
     繰り返し最小二乗アルゴリズムのループ部分
     '''
     # 解との偏差のベクトルr~を計算する
     r_tilde = phi.dot(theta) - y
-    # 重みベクトルw~を計算する(r_tildeの要素を置き換える形で)
-    np.where((r_tilde < eta) and (-eta < r_tilde), (1 - r_tilde**2 / eta**2)**2, 0)
+    # 重みベクトルw~を計算する
+    w_tilde = np.where((r_tilde < eta) & (-eta < r_tilde), (1 - r_tilde**2 / eta**2)**2, 0)
+    # w~により計算される対角行列W~を計算する
+    W_tilde = np.diag(w_tilde)
+    # thetaを重み付き最小二乗回帰の解析解を用いて更新する
+    return np.linalg.inv(phi.T.dot(W_tilde.dot(phi))).dot(phi.T.dot(W_tilde).dot(y))
 
 
 def iterative_reweighted_least_squares(x, y, eta=1., n_iter=1000):
     # create design matrix
     phi = build_design_matrix(x)
     # initialize theta
-    theta = np.empty_like(x)
+    # 基底関数が2つなのでパラメータは2つ
+    theta = np.empty(2)
+    # ループ計算により解を漸近させる
+    for _ in range(n_iter):
+        theta_new = loop(x, y, phi, theta, eta)
+        if np.linalg.norm(theta - theta_new) < 1e-3:
+            theta = theta_new
+            break
+        else:
+            theta = theta_new
+    return theta
 
 
 if __name__ == '__main__':
-    x = np.empty(10)
-    print(build_design_matrix(x))
+    # create dataset
+    x, y = generate_sample()
+    theta = iterative_reweighted_least_squares(x, y)
